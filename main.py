4442357f2fd910bc476472c4f19c4ddd708a74af
Eric Duminil <eric.duminil@gmail.com>
2025-12-07 22:52:07 +0100

    different layout

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import insel
 import streamlit as st
 
+st.set_page_config(layout="wide")
 st.title("INSEL online")
-lon = st.slider("Longitude", -180, 180, 30)
-lat = st.slider("Latitude", -90, 90, 30)
-result = insel.block("SUM", lon, lat)
+
+col1, col2 = st.columns([1, 2])
+with col1:
+    lon = st.slider("Longitude", -180, 180, 30)
+    lat = st.slider("Latitude", -90, 90, 30)
+
+with col2:
+    result = insel.block("SUM", lon, lat)
+    st.write(f"{lon} + {lat} = {result}")
+
 
 # insel.template('sunorb.vseit', Longitude=lon, Latitude=lat, Timezone=timezone)
-st.write(f"{lon} + {lat} = {result}")
 # st.image("/tmp/sunorb.png")
 
