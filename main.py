3011968ed8338919a6452a5ce302005829950631
Eric Duminil <eric.duminil@gmail.com>
2025-12-07 22:39:02 +0100

    Removing debug lines

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import insel
 import streamlit as st
 
-print("Hello from streamlit-insel!")
-
 st.title("INSEL online")
 lon = st.slider("Longitude", -180, 180, 30)
 lat = st.slider("Latitude", -90, 90, 30)
@@ -10,6 +8,5 @@ result = insel.block("SUM", lon, lat)
 
 # insel.template('sunorb.vseit', Longitude=lon, Latitude=lat, Timezone=timezone)
 st.write(f"{lon} + {lat} = {result}")
-print("Done")
 # st.image("/tmp/sunorb.png")
 
