0d4dec3ae2a9bced17f0788b4f1984e40e7ccdf4
Eric Duminil <eric.duminil@gmail.com>
2025-12-08 08:35:26 +0100

    Plot is working!

--- main.py
+++ main.py
@@ -6,14 +6,24 @@ st.title("INSEL online")
 
 col1, col2 = st.columns([1, 1])
 with col1:
-    lon = st.slider("Longitude", -180, 180, 30)
-    lat = st.slider("Latitude", -90, 90, 30)
+    st.title("Bezug  💥👽👽👽👽👽 ")
+    verbrauch = st.slider("🔌 Verbrauch [MWh/a]", 0, 50, 10)
+    pvleistung = st.slider("🌞 PV Leistung [kWp]", 0, 50, 10)
+    wirkungsgrad = st.slider("🔋 Batteriewirkungsgrad [%]", 0, 100, 95)
+    kapazitaetbatterie = st.slider("🔋 Batteriekapazitaet [kWh]", 0, 50, 5)
 
 with col2:
-    result = insel.template("Last_PV_Batterie.vseit", x=lon, y=lat)
-    st.write(f"{lon} + {lat} = {result}")
+    eigenverbrauchsquote, autarkiequote = insel.template(
+        "Last_PV_Batterie.vseit",
+        MWh_Verbrauch=verbrauch,
+        kWp_PV=pvleistung,
+        Kapazitaet_Batterie=kapazitaetbatterie,
+        Wirkungsgrad_Batterie=wirkungsgrad / 100,
+    )
+
+    st.write(f" Eigenverbrauchsquote = {round(eigenverbrauchsquote*100)} %")
+    st.write(f" Autarkiequote 🗾 = {round(autarkiequote*100)} %")
     st.image("/tmp/Last_PV_Batterie.png")
 
 
 # insel.template('sunorb.vseit', Longitude=lon, Latitude=lat, Timezone=timezone)
-
