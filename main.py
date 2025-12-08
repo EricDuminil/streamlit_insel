d96d49667e1dec80fdad2aaba6645b3f41b24569
Eric Duminil <eric.duminil@gmail.com>
2025-12-08 08:32:41 +0100

    Displaying plot

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 st.title("INSEL online")
 
-col1, col2 = st.columns([1, 2])
+col1, col2 = st.columns([1, 1])
 with col1:
     lon = st.slider("Longitude", -180, 180, 30)
     lat = st.slider("Latitude", -90, 90, 30)
@@ -12,8 +12,8 @@ with col1:
 with col2:
     result = insel.template("Last_PV_Batterie.vseit", x=lon, y=lat)
     st.write(f"{lon} + {lat} = {result}")
+    st.image("/tmp/Last_PV_Batterie.png")
 
 
 # insel.template('sunorb.vseit', Longitude=lon, Latitude=lat, Timezone=timezone)
-# st.image("/tmp/sunorb.png")
 
